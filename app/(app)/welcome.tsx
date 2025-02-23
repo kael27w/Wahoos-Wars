cc9c8be0d37e6f25e1a3b6cc63a943b72a53b020
Ryan Nguyen <rnguyen513@gmail.com>
2025-02-23 14:01:11 -0500

    Revert "merge"
    This reverts commit 0422c12fc482ab19b72d7bc2cedee772612e776e, reversing
    changes made to 78390b3034724de63538fd1c285bd76442a3fba4.

--- app/(app)/welcome.tsx
+++ app/(app)/welcome.tsx
@@ -15,12 +15,13 @@ export default function WelcomeScreen() {
 		<SafeAreaView className="flex flex-1 bg-background p-4">
 			<View className="flex flex-1 items-center justify-center gap-y-4 web:m-4">
 				<Image
-					source={require("@/assets/wwlogo.png")}
-					className="h-[11rem] w-[20rem] mt-[3rem]"
+					source={require("@/assets/icon.png")}
+					className="w-16 h-16 rounded-xl"
 				/>
-				<H1 className="text-center">Welcome to Wahoo Wars!</H1>
+				<H1 className="text-center">Welcome to TRIVIA</H1>
 				<Muted className="text-center">
-				Compete with other Hoos to become the greatest of them all! Are you a trivia master? An athletic prodigy? A creative genius? No matter your talent, this is your chance to shine and prove you're the best of the best. Whether you're sprinting to victory on the track, solving complex puzzles, or dazzling everyone with your artistic skills, every challenge is an opportunity to show your true colors. So, gather your team, hone your skills, and get ready to push your limits. The road to greatness is paved with challenges, but with determination and a bit of friendly competition, you'll rise to the top. Will you seize the moment and become the greatest of them all?
+					A comprehensive starter project for developing React Native and Expo
+					applications with Supabase as the backend.
 				</Muted>
 			</View>
 			<View className="flex flex-col gap-y-4 web:m-4">
